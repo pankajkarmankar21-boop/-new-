6d06a57c6bd881996a6f0fe40a40133764994a37
pankajkarmankar21-boop <pankajkarmankar21@gmail.com>
2026-07-21 19:15:00 +0530

    Delete src directory

--- src/app/farmer/notifications/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { NotificationsList } from "@/components/NotificationsList";
-
-export default function FarmerNotificationsPage() {
-  return <NotificationsList backHref="/farmer/home" themeGradient="from-primary-600 to-emerald-600" />;
-}
